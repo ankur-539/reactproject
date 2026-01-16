8a082f6928ce4c9439853575646b61cffa4bbb13
ankur <aankur00000@gmail.com>
2026-01-17 00:25:55 +0530

    reactpro

--- src/Basic.js
+++
@@ -1,5 +0,0 @@
-function Abc(){
-    return "welcome to reactJS"
-}
-
-export default Abc;
--- src/index.js
+++ src/index.js
@@ -1,13 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-// import Abc from './Basic';
-import Registerform from './Register';
-import './Style.css'
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    {/* <Abc></Abc> */}
-    <Registerform></Registerform>
+
   </React.StrictMode>
 );
