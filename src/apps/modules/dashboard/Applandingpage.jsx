92323d0efdf092d847e7c3327db185678ae8fbc1
ankur <aankur00000@gmail.com>
2026-01-29 14:37:55 +0530

    change

--- src/apps/modules/dashboard/Applandingpage.jsx
+++ src/apps/modules/dashboard/Applandingpage.jsx
@@ -1,158 +1,158 @@
-import React from "react";
-import { VscLayoutSidebarLeft } from "react-icons/vsc";
-import { FaHome } from "react-icons/fa";
-import { FaFolderClosed } from "react-icons/fa6";
-import { IoCalendarNumber } from "react-icons/io5";
-import { RiTeamFill } from "react-icons/ri";
-import { FaChartPie } from "react-icons/fa";
-import { AiFillSetting } from "react-icons/ai";
-import { FaSearch } from "react-icons/fa";
-import { FaRegBell } from "react-icons/fa";
-import { IoLogoFacebook } from "react-icons/io5";
-import { IoLogoInstagram } from "react-icons/io";
-import { RiTwitterXLine } from "react-icons/ri";
-import { ImGithub } from "react-icons/im";
-import { FaYoutube } from "react-icons/fa6";
-import { Link } from "react-router-dom";
+// import React from "react";
+// import { VscLayoutSidebarLeft } from "react-icons/vsc";
+// import { FaHome } from "react-icons/fa";
+// import { FaFolderClosed } from "react-icons/fa6";
+// import { IoCalendarNumber } from "react-icons/io5";
+// import { RiTeamFill } from "react-icons/ri";
+// import { FaChartPie } from "react-icons/fa";
+// import { AiFillSetting } from "react-icons/ai";
+// import { FaSearch } from "react-icons/fa";
+// import { FaRegBell } from "react-icons/fa";
+// import { IoLogoFacebook } from "react-icons/io5";
+// import { IoLogoInstagram } from "react-icons/io";
+// import { RiTwitterXLine } from "react-icons/ri";
+// import { ImGithub } from "react-icons/im";
+// import { FaYoutube } from "react-icons/fa6";
+// import { Link } from "react-router-dom";
 
 
-function Applandingpage() {
-  return (
-    <div className="container-fluid overflow-hidden">
-      <div className="row d-flex ">
-        {/**************start side bar*******************/}
-        <div className="col-1 g-0 w-auto d-flex">
-          <div className="hight width px-2 d-flex flex-column align-items-center gap-4 py-3 c-bg-3 text-white shadow">
-            <VscLayoutSidebarLeft className="fs-3" data-bs-toggle="collapse" data-bs-target="#collapseWidthExample"
-              aria-expanded="false" aria-controls="collapseWidthExample" />
-            <hr className="w-100 m-0" />
-            <FaHome></FaHome>
-            <RiTeamFill></RiTeamFill>
-            <FaFolderClosed></FaFolderClosed>
-            <IoCalendarNumber></IoCalendarNumber>
-            <FaChartPie></FaChartPie>
-            <AiFillSetting className="mt-auto mb-2 fs-5"></AiFillSetting>
-          </div>
+// function Applandingpage() {
+//   return (
+//     <div className="container-fluid overflow-hidden">
+//       <div className="row d-flex ">
+//         {/**************start side bar*******************/}
+//         <div className="col-1 g-0 w-auto d-flex">
+//           <div className="hight width px-2 d-flex flex-column align-items-center gap-4 py-3 c-bg-3 text-white shadow">
+//             <VscLayoutSidebarLeft className="fs-3" data-bs-toggle="collapse" data-bs-target="#collapseWidthExample"
+//               aria-expanded="false" aria-controls="collapseWidthExample" />
+//             <hr className="w-100 m-0" />
+//             <FaHome></FaHome>
+//             <RiTeamFill></RiTeamFill>
+//             <FaFolderClosed></FaFolderClosed>
+//             <IoCalendarNumber></IoCalendarNumber>
+//             <FaChartPie></FaChartPie>
+//             <AiFillSetting className="mt-auto mb-2 fs-5"></AiFillSetting>
+//           </div>
 
-          <div class="collapse collapse-horizontal w-75" id="collapseWidthExample">
-            <div class="card card-body rounded-0 border-0 hight c-bg-4 gap-3">
-              <p className="m-0 fw-bold fm text-light fs-4 fw-bold">AppName</p>
-              <hr className="w-100 m-0 mb-1 text-light">
-              </hr>
-              <p className="m-0 fw-bold fm text-light abc">Dashboard</p>
-              <p className="m-0 fw-bold fm text-light abc">Team</p>
-              <p className="m-0 fw-bold fm text-light abc">Project</p>
-              <p className="m-0 fw-bold fm text-light abc">Calendar</p>
-              <p className="m-0 fw-bold fm text-light abc">Report</p>
-              <button type="button" class="btn btn-outline-light m-0 mt-auto">
-                + add new entry
-              </button>
-            </div>
-          </div>
-        </div>
-        {/**************end side bar*******************/}
+//           <div class="collapse collapse-horizontal w-75" id="collapseWidthExample">
+//             <div class="card card-body rounded-0 border-0 hight c-bg-4 gap-3">
+//               <p className="m-0 fw-bold fm text-light fs-4 fw-bold">AppName</p>
+//               <hr className="w-100 m-0 mb-1 text-light">
+//               </hr>
+//               <p className="m-0 fw-bold fm text-light abc">Dashboard</p>
+//               <p className="m-0 fw-bold fm text-light abc">Team</p>
+//               <p className="m-0 fw-bold fm text-light abc">Project</p>
+//               <p className="m-0 fw-bold fm text-light abc">Calendar</p>
+//               <p className="m-0 fw-bold fm text-light abc">Report</p>
+//               <button type="button" class="btn btn-outline-light m-0 mt-auto">
+//                 + add new entry
+//               </button>
+//             </div>
+//           </div>
+//         </div>
+//         {/**************end side bar*******************/}
 
-        <div className="col-1 g-0 flex-grow-1">
-          {/**************start search bar*******************/}
-          <div className=" shadow d-flex align-items-center px-4 py-3 c-bg-4 text-white hight-3">
-            <FaSearch className='fs-5'></FaSearch>
-            <input class="form-control border-0 w-50 mx-3 " type="search" placeholder="Search" aria-label="Search" />
-            <FaRegBell className='ms-auto me-4 c-pointer fs-4'></FaRegBell>
-            <div className='pro me-3'></div>
-            <div class="dropdown text-white">
-              <div class="dropdown-toggle c-pointer" data-bs-toggle="dropdown" aria-expanded="false">UserName</div>
-              <ul class="dropdown-menu mt-3">
-                <li><Link to="" class="dropdown-item" href="#">User profile</Link></li>
-                <li><Link to="" class="dropdown-item" href="#">Sign out</Link></li>
-              </ul>
-            </div>
-          </div>
-          {/**************end search bar*******************/}
+//         <div className="col-1 g-0 flex-grow-1">
+//           {/**************start search bar*******************/}
+//           <div className=" shadow d-flex align-items-center px-4 py-3 c-bg-4 text-white hight-3">
+//             <FaSearch className='fs-5'></FaSearch>
+//             <input class="form-control border-0 w-50 mx-3 " type="search" placeholder="Search" aria-label="Search" />
+//             <FaRegBell className='ms-auto me-4 c-pointer fs-4'></FaRegBell>
+//             <div className='pro me-3'></div>
+//             <div class="dropdown text-white">
+//               <div class="dropdown-toggle c-pointer" data-bs-toggle="dropdown" aria-expanded="false">UserName</div>
+//               <ul class="dropdown-menu mt-3">
+//                 <li><Link to="" class="dropdown-item" href="#">User profile</Link></li>
+//                 <li><Link to="" class="dropdown-item" href="#">Sign out</Link></li>
+//               </ul>
+//             </div>
+//           </div>
+//           {/**************end search bar*******************/}
 
-          {/**************start containt area*******************/}
+//           {/**************start containt area*******************/}
 
-          <div className="container-fluid hight-1 bg-info overflow-y-auto">
-            <div className="row mt-3">
-              <div className="col-md-3">
-                <div class="card text-center mb-3 hight-5">
-                  <div class="card-body">
+//           <div className="container-fluid hight-1 bg-info overflow-y-auto">
+//             <div className="row mt-3">
+//               <div className="col-md-3">
+//                 <div class="card text-center mb-3 hight-5">
+//                   <div class="card-body">
 
-                  </div>
-                </div>
-              </div>
-              <div className="col-md-3">
-                <div class="card text-center mb-3 hight-5">
-                  <div class="card-body">
+//                   </div>
+//                 </div>
+//               </div>
+//               <div className="col-md-3">
+//                 <div class="card text-center mb-3 hight-5">
+//                   <div class="card-body">
 
-                  </div>
-                </div>
-              </div>
-              <div className="col-md-3">
-                <div class="card text-center mb-3 hight-5">
-                  <div class="card-body">
+//                   </div>
+//                 </div>
+//               </div>
+//               <div className="col-md-3">
+//                 <div class="card text-center mb-3 hight-5">
+//                   <div class="card-body">
 
-                  </div>
-                </div>
-              </div>
-              <div className="col-md-3">
-                <div class="card text-center mb-3 hight-5">
-                  <div class="card-body">
+//                   </div>
+//                 </div>
+//               </div>
+//               <div className="col-md-3">
+//                 <div class="card text-center mb-3 hight-5">
+//                   <div class="card-body">
 
-                  </div>
-                </div>
-              </div>
-              <div className="col-md-8">
-                <div class="card text-center mb-3 hight-4">
-                  <div class="card-body">
+//                   </div>
+//                 </div>
+//               </div>
+//               <div className="col-md-8">
+//                 <div class="card text-center mb-3 hight-4">
+//                   <div class="card-body">
 
-                  </div>
-                </div>
-              </div>
-              <div className="col-md-4">
-                <div class="card text-center mb-3 hight-4">
-                  <div class="card-body">
+//                   </div>
+//                 </div>
+//               </div>
+//               <div className="col-md-4">
+//                 <div class="card text-center mb-3 hight-4">
+//                   <div class="card-body">
 
-                  </div>
-                </div>
-              </div>
-              <div className="col-md-6">
-                <div class="card text-center mb-3 hight-6">
-                  <div class="card-body">
+//                   </div>
+//                 </div>
+//               </div>
+//               <div className="col-md-6">
+//                 <div class="card text-center mb-3 hight-6">
+//                   <div class="card-body">
 
-                  </div>
-                </div>
-              </div>
-              <div className="col-md-3">
-                <div class="card text-center mb-3 hight-6">
-                  <div class="card-body">
+//                   </div>
+//                 </div>
+//               </div>
+//               <div className="col-md-3">
+//                 <div class="card text-center mb-3 hight-6">
+//                   <div class="card-body">
 
-                  </div>
-                </div>
-              </div>
-              <div className="col-md-3">
-                <div class="card text-center mb-3 hight-6">
-                  <div class="card-body">
+//                   </div>
+//                 </div>
+//               </div>
+//               <div className="col-md-3">
+//                 <div class="card text-center mb-3 hight-6">
+//                   <div class="card-body">
 
-                  </div>
-                </div>
-              </div>
-            </div>
-          </div>
-          {/**************end containt area*******************/}
+//                   </div>
+//                 </div>
+//               </div>
+//             </div>
+//           </div>
+//           {/**************end containt area*******************/}
 
-          {/**************start footer*******************/}
-          <div className="c-bg-4 text-light shadow d-flex align-items-center gap-4 p-4 hight-2">
-            <div>©Company</div>
-            <IoLogoFacebook className='ms-auto fs-5 c-pointer opacity-75'></IoLogoFacebook>
-            <IoLogoInstagram className='fs-5 c-pointer opacity-75'></IoLogoInstagram>
-            <RiTwitterXLine className='fs-5 c-pointer opacity-75'></RiTwitterXLine>
-            <ImGithub className='fs-5 c-pointer opacity-75'></ImGithub>
-            <FaYoutube className='fs-5 c-pointer opacity-75'></FaYoutube>
-          </div>
-        </div>
-        {/**************start footer*******************/}
-      </div>
-    </div>
-  );
-};
-export default Applandingpage;
+//           {/**************start footer*******************/}
+//           <div className="c-bg-4 text-light shadow d-flex align-items-center gap-4 p-4 hight-2">
+//             <div>©Company</div>
+//             <IoLogoFacebook className='ms-auto fs-5 c-pointer opacity-75'></IoLogoFacebook>
+//             <IoLogoInstagram className='fs-5 c-pointer opacity-75'></IoLogoInstagram>
+//             <RiTwitterXLine className='fs-5 c-pointer opacity-75'></RiTwitterXLine>
+//             <ImGithub className='fs-5 c-pointer opacity-75'></ImGithub>
+//             <FaYoutube className='fs-5 c-pointer opacity-75'></FaYoutube>
+//           </div>
+//         </div>
+//         {/**************start footer*******************/}
+//       </div>
+//     </div>
+//   );
+// };
+// export default Applandingpage;
